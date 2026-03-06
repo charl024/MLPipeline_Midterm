fba8ccb6a4585149192926c8f6107edd3df74d55
charl024 <comaoeng2003@gmail.com>
2026-03-05 17:29:58 -0700

    Added getter functions for datasets, also flattens them

---
+++ import_datasets.py
@@ -0,0 +1,27 @@
+import idx2numpy
+
+# returns training data/labels, testing data/labels, flattens data
+def get_fashion_mnist():
+    train_images = idx2numpy.convert_from_file("./datasets/FashionMNIST/raw/train-images-idx3-ubyte")
+    train_labels = idx2numpy.convert_from_file("./datasets/FashionMNIST/raw/train-labels-idx1-ubyte")
+    test_images = idx2numpy.convert_from_file("./datasets/FashionMNIST/raw/t10k-images-idx3-ubyte")
+    test_labels = idx2numpy.convert_from_file("./datasets/FashionMNIST/raw/t10k-labels-idx1-ubyte")
+
+    train_flattened = train_images.reshape(train_images.shape[0], -1)
+    test_flattened = test_images.reshape(test_images.shape[0], -1)
+
+    return train_images, train_labels, test_images, test_labels
+
+def get_mnist():
+    train_images = idx2numpy.convert_from_file("./datasets/MNIST/raw/train-images-idx3-ubyte")
+    train_labels = idx2numpy.convert_from_file("./datasets/MNIST/raw/train-labels-idx1-ubyte")
+    test_images = idx2numpy.convert_from_file("./datasets/MNIST/raw/t10k-images-idx3-ubyte")
+    test_labels = idx2numpy.convert_from_file("./datasets/MNIST/raw/t10k-labels-idx1-ubyte")
+
+    train_flattened = train_images.reshape(train_images.shape[0], -1)
+    test_flattened = test_images.reshape(test_images.shape[0], -1)
+
+    return train_images, train_labels, test_images, test_labels
+
+print(get_fashion_mnist())
+print(get_mnist())
