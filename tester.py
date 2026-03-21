d88eea01199b643f4b653bf430d1b6a173a72cfe
Joel Villarreal II <joel.villa2329@gmail.com>
2026-03-21 12:55:17 -0600

    better tests

--- tester.py
+++ tester.py
@@ -13,7 +13,8 @@ from sklearn.model_selection import validation_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC 
 
-NUM_ITER = 8192
+NUM_ITER = 32
+NUM_COMPONENTS = [50, 100, 200]
 
 def test():
     train_data, train_labels, test_data, test_labels = get_mnist()
@@ -35,66 +36,118 @@ def calc_errors(pipeline):
     
     return train_error, test_error
 
+def update_best(arr, x, C, gamma=0, degree=0):
+    """
+    Update and return array, based on if x value and arr[0]
+    """
+    if (arr[0] < x):
+        arr[0] = x
+        arr[1] = C
+        arr[2] = gamma
+        arr[3] = degree
+    return arr
+
+def single_test(svc, num_comp):
+    """
+    Creates a single model, times it, tracks error
+    """
+    pl = Pipeline(SVC=svc, num_components=num_comp)
+    time = pl.fit()
+
+    train_error, test_error = calc_errors(pl)
+    train_accuracy = (1.0 - train_error)
+    test_accuracy  = (1.0 - test_error)
+
+    # Print accuracies
+    print(f"training accuracy = {train_accuracy}, test accuracy = {test_accuracy}, fit time = {time}\n")
+
+    return test_accuracy
+
 def linear_kernel_test():
     C_param_range = [0.0001, 0.001, 0.01, 0.1, 1]
 
     print(f"Number of iterations: {NUM_ITER}\n")
 
+    for num_comp in NUM_COMPONENTS:
+        print(f"Number of components for PCA reduction = {num_comp}\n")
 
-    for C in C_param_range:
-        svc = SVC(kernel='linear', max_iter=NUM_ITER, C=C)
-        pl = Pipeline(SVC=svc)
-        total = pl.fit()
+        test_best = [0, 0, 0, 0]
 
-        train_error, test_error = calc_errors(pl)
+        for C in C_param_range:
+            # Print hyper params
+            print(f"C = {C}")
 
-        train_accuracy = (1.0 - train_error)
-        test_accuracy  = (1.0 - test_error)
+            # Make Model
+            svc = SVC(kernel='linear', max_iter=NUM_ITER, C=C)
+            test_acc = single_test(svc=svc, num_comp=num_comp)
 
-        print(f"C = {C}")
-        print(f"training accuracy = {train_accuracy}, test accuracy = {test_accuracy}, fit time = {total}\n")
+            # Track the best test accuracy
+            test_best = update_best(test_best, test_acc, C)
+        
+        #OUTPUT BEST on test data set
+        print(f"Test Best: C = {test_best[1]}, test accuracy: {test_best[0]}\n")
 
 def rbf_kernel_test():
     C_param_range = [0.1, 1, 10, 100, 1000]
     gamma_param_range = [0.0001, 0.001, 0.01, 0.1, 1]
 
     print(f"Number of iterations: {NUM_ITER}\n")
+
+    for num_comp in NUM_COMPONENTS:
+        print(f"Number of components for PCA reduction = {num_comp}\n")
+
+        test_best = [0, 0, 0, 0]
+
+        for C in C_param_range:
+            for gamma in gamma_param_range:
+                # Print hyper params
+                print(f"C = {C}, gamma = {gamma}")
+
+                # Make Model
+                svc = SVC(kernel='rbf', max_iter=NUM_ITER, C=C, gamma=gamma)
+                test_acc = single_test(svc=svc, num_comp=num_comp)
+                # Track the best test accuracy
+                test_best = update_best(test_best, test_acc, C, gamma)
         
-    train_best = [0, 0, 0]
-    test_best = [0, 0, 0]
-    best = [0, 0, 0]
+        #OUTPUT BEST on test data set
+        print(f"Test Best: C = {test_best[1]}, gamma = {test_best[2]}, test accuracy: {test_best[0]}\n")
 
+def poly_kernel_test():
+    # C = 0.1, gamma = 0.01, degree = 3 did best
+    C_param_range = [0.001, 0.01, 0.1, 1, 10]
+    gamma_param_range = [0.0001, 0.001, 0.01, 0.1, 1]
+    degree_param_range = [1, 2, 3, 4, 5]
 
-    for C in C_param_range:
-        for gamma in gamma_param_range:
-            svc = SVC(kernel='rbf', max_iter=NUM_ITER, C=C, gamma=gamma)
-            pl = Pipeline(SVC=svc)
-            total = pl.fit()
+    print(f"Number of iterations: {NUM_ITER}\n")
 
-            train_error, test_error = calc_errors(pl)
+    for num_comp in NUM_COMPONENTS:
+        print(f"Number of components for PCA reduction = {num_comp}\n")
 
-            train_accuracy = (1.0 - train_error)
-            test_accuracy  = (1.0 - test_error)
+        test_best = [0, 0, 0, 0]
 
-            if (train_best[0] < train_accuracy):
-                train_best[0] = train_accuracy
-                train_best[1] = C
-                train_best[2] = gamma
+        for C in C_param_range:
+            for gamma in gamma_param_range:
+                for degree in degree_param_range:
+                    # Print hyper params
+                    print(f"C = {C}, gamma = {gamma}, degree = {degree}")
 
-            if (test_best[0] < test_accuracy):
-                test_best[0] = test_accuracy
-                test_best[1] = C
-                test_best[2] = gamma
+                    # Make Model
+                    svc = SVC(kernel='poly', 
+                              max_iter=NUM_ITER, 
+                              C=C, gamma=gamma, 
+                              degree=degree)
+                    
+                    test_acc = single_test(svc=svc, num_comp=num_comp)
 
-            if (best[0] < (test_accuracy + train_accuracy) / 2):
-                best[0] = (test_accuracy + train_accuracy) / 2
-                best[1] = C
-                best[2] = gamma
+                    # Track the best test accuracy
+                    best_tracker = update_best(best_tracker, 
+                                               test_acc, 
+                                               C=C, 
+                                               gamma=gamma, 
+                                               degree=degree)
+        
+        #OUTPUT BEST on test data set
+        print(f"Test Best: C = {test_best[1]}, gamma = {test_best[2]}, test accuracy: {test_best[0]}\n")
 
-            print(f"C = {C}, gamma = {gamma}")
-            print(f"training accuracy = {train_accuracy}, test accuracy = {test_accuracy}, fit time = {total}\n")
-    print(f"Train Best: C = {train_best[1]}, gamma = {train_best[2]}, training accuracy: {train_best[0]}")
-    print(f"Test Best: C = {test_best[1]}, gamma = {test_best[2]}, test accuracy: {test_best[0]}")
-    print(f"Best Average: C = {best[1]}, gamma = {best[2]}, (test + train) / 2 : {best[0]}")
-
-rbf_kernel_test()
+if __name__ == "__main__":
+    linear_kernel_test()
